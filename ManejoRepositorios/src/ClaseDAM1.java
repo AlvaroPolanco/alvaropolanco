a9cd7e69cc44640a31522504c9e2d5d205e44dd5
usuario <usuario@usuario-PC>
2017-12-15 16:09:11 +0100

    jhvh

---
+++ ManejoRepositorios/src/ClaseDAM1.java
@@ -0,0 +1,42 @@
+
+public class ClaseDAM1 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Cada estudiante aÃ±adirÃ¡ su nombre.
+		String profesor = "Luis de Blas";
+
+		String al01_SergioAmor = "Sergio Amor Gutiérrez";
+		String al02_JorgeArroy = "";
+		String al03_ManuelBoo = "";
+		String al04_JoseDaBuenaga = "";
+		String al05_DavidCicero = "";
+		String al06_NinaCimadevilla = "";
+		String al07_DanielDehesa = "";
+		String al09_JonatanDelRio = "";
+		String al11_VictorDiaz = "";
+		String al12_VictorFernandez = "";
+		String al13_MarioFernandez = "";
+		String al16_JonatanBordetas = "Jonathan Gómez Bordetas";
+		String al17_AnaGonzalez = "Ana Maria Gonzalez Rodriguez";
+		String al18_IkerIriondo = "";
+		String al19_DavidMartinez = "";
+		String al20_AdrianNoriega = "";
+		String al21_DiegoPerez = "";
+		String al22_RubenPerez = "";
+		String al23_MarioPerez = "Mario Pérez Urbaneja";
+		String al24_ChristianPinera= "";
+		String al25_AlvaroPolanco = "Alvaro Polanco Rodriguez";
+		String al26_ElenaPuig = "";
+		String al27_JesusRuiz = "";
+		String al28_PabloSanchez= "";
+		String al30_MarcosSuarez = "";
+		String al31_DiegoVelez = "";
+		String al32_MarcosGonzalez= "";
+
+
+	}
+
+}
+
+
